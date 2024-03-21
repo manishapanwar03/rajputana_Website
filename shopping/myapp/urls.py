b356575a6e200afac410ea5663cf4aeed9662190
bridgefix <bridgefix@gmail.com>
2024-03-21 12:06:28 +0530

    added migrations and urls.py

--- shopping/myapp/urls.py
+++ shopping/myapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('dress/<int:pk>/', CreateDress.as_view(), name="dress_id"),
     path('jewellary/', CreateJwellery.as_view(), name="dress"),
     path('jewellary/<int:pk>/', CreateJwellery.as_view(), name="dress_id"),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
